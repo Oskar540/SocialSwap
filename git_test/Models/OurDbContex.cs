a1e922e90894cf54b6b49e380251349df997165e
kamson <kamson785@gmail.com>
2017-05-14 15:08:21 +0200

    Logowanie  i Rejestracja
    Stworzenie Metod logowania i rejestracji użytkownika,zainstalowania
    Frameworka Entity

---
+++ git_test/Models/OurDbContex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace git_test.Models
+{
+    public class OurDbContex : DbContext
+    {
+        public DbSet<UserAccount> userAccount { get; set; }
+    }
+}
