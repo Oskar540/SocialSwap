6166c7095bc6e76d6151d62c9551851d5e17f361
kamson <kamson785@gmail.com>
2017-05-20 12:48:47 +0200

    final

--- git_test/Models/UserAccount.cs
+++ git_test/Models/UserAccount.cs
@@ -17,26 +17,33 @@ namespace git_test.Models
         public string LastName { get; set; }
         [Required(ErrorMessage = "Email jest wymagany")]
         [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3]\.)|(([\w-]+\.)+))([a-zA-Z{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Wpisz poprawny email")]
+        [Display(Name = "Email")]
         public string Email { get; set; }
-        [Display(Name = "Nazwa")]
         [Required(ErrorMessage = "Podaj Nick")]
+        [Display(Name = "Nazwa")]
         public string Username { get; set; }
         [Required(ErrorMessage = "Wpisz hasło")]
         [MinLength(8,ErrorMessage ="Hasło za krótkie")]
         [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
         public string Password { get; set; }
         
         [Compare("Password",ErrorMessage = "Hasła nie pasują")]
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "Potwierdź hasło")]
+        [Display(Name = "Potwierdź hasło")]
         public string ConfirmPassword { get; set; }
         [Required(ErrorMessage = "to pole jest wymagane")]
+        [Display(Name = "Opis")]
         public string Describe { get; set; }
         [Required(ErrorMessage = "to pole jest wymagane")]
+        [Display(Name = "Lokacja")]
         public string Location { get; set; }
         [Required(ErrorMessage = "to pole jest wymagane")]
+        [Display(Name = "Numer")]
         public string Numer { get; set; }
         [Required(ErrorMessage = "to pole jest wymagane")]
+        [Display(Name = "Hobby")]
         public string Hobby { get; set; }
 
 
