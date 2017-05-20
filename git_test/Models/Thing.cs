cac809572172c4d6a18b2c49442be39cdfb119a6
kamson <kamson785@gmail.com>
2017-05-20 13:31:51 +0200

    Final

--- git_test/Models/Thing.cs
+++ git_test/Models/Thing.cs
@@ -16,10 +16,13 @@ namespace git_test.Models
         public int ThingId { get; set; }
         public string Username { get; set; }
         [MaxLength(25,ErrorMessage = "Nazwa moze miec tylko 25 znakow")]
+        [Display(Name = "Nazwa")]
         public string Name { get; set; }
         [Required]
+        [Display(Name = "Opis")]
         public string Describe { get; set; }
         [Required]
+        [Display(Name = "Tagi")]
         public string Tags { get; set; }
 
     }
